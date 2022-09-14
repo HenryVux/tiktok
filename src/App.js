eb465df06368abfe3ec6638b1aa41ff77e0a8d6b
Henry Vu <vuhoan528@gmail.com>
2022-09-14 17:12:17 +0700

    62.Dựng khung Default Layout
    setup lib classNames: npm i classnames
    
    import classNames from 'classnames/bind';
    import styles from './Header.module.scss';
    const cx = classNames.bind(styles);

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './components/Button';
-import Button from './components/Button';
+// import Button from './components/Button';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/components/Layout/';
 import { Fragment } from 'react';
