a70f259c8560f7a3e0cfd1e522117922d31ad364
Henry Vu <vuhoan528@gmail.com>
2022-09-14 14:34:53 +0700

    60.config CSS, SASS
    create GlobalStyles/GlobalStyles.scss component
    setup SASS: npm i -D sass
    reset CSS: npm install --save normalize.css
    default css: font-family, font-size, line-height
    https://fonts.google.com/specimen/Montserrat#styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import './components/Button';
 import Button from './components/Button';
 
@@ -7,15 +5,7 @@ function App() {
     return (
         <div className="App">
             <Button />
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+            <h2>React App</h2>
         </div>
     );
 }
