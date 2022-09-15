9687622b065d5e53c3924a564932ec65f8f5515f
Henry Vu <vuhoan528@gmail.com>
2022-09-15 11:28:56 +0700

    63.Header 1
    bổ sung GlobalStyles/GlobalStyles/index.js và import vào App/index.js
    <GlobalStyles>
    	<App />
    </GlobalStyles>
    ...json:
    "@fortawesome/fontawesome-svg-core": "^1.3.0",
    "@fortawesome/free-brands-svg-icons": "^6.0.0",
    "@fortawesome/free-regular-svg-icons": "^6.0.0",
    "@fortawesome/free-solid-svg-icons": "^6.0.0",
    "@fortawesome/react-fontawesome": "^0.1.17",
    
    paste to file 'package.json' - run 'npm i'

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,15 +1,32 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    // console.log(images.logo);
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                {/* logo */}
-                {/* search */}
-                {/* button */}
+                <div className={cx('logo')}>
+                    {/* <use xlink:href="#svg-header-logo"></use> */}
+                    <img src={images.logo} alt="tiktok logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className={cx('action')}></div>
             </div>
         </header>
     );
